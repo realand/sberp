18a4109cb82fbd0769260837df5eb42df1aaa93b
realand <50632079+realand@users.noreply.github.com>
2023-10-24 08:25:01 +0300

    fix: newline
    fix: new line in include.php

--- include.php
+++ include.php
@@ -3,14 +3,11 @@
 header("Content-type: text/html; charset=utf-8"); 
 
 include("config.php");
-
 include("helpers/function.php");
 include("helpers/query.php");
-
 include("helpers/order.php");
 include("helpers/order_log.php");
 include("helpers/payment.php");
-
 include("helpers/core.php");
 
 
